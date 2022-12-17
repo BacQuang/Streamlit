cacfe4be1c7e12608aa980a9c373e31c3cda19d4
BacQuang <117609223+BacQuang@users.noreply.github.com>
2022-12-17 14:54:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
 from io import BytesIO
-import vnquant.data as dt 
+import vnquantdatadownload.vnquant.data as dt 
 	
 
 st.set_page_config(page_title = "Nguyen Quang Bac's Stock Portfolio Optimizer", layout = "wide")
