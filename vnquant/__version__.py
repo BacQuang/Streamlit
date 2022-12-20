047ada18765a213ef1588e4194f271706953d3cc
BacQuang <117609223+BacQuang@users.noreply.github.com>
2022-12-20 22:42:46 +0700

    Delete vnquant directory

--- vnquant/__version__.py
+++
@@ -1,2 +0,0 @@
-﻿# Copyright (c) general_backbone. All rights reserved.
-__version__ = '0.0.3'
--- vnquant/plot/__init__.py
+++
@@ -1,6 +0,0 @@
-﻿# Copyright (c) general_backbone. All rights reserved.
-from .Plot import _vnquant_candle_stick_source, _vnquant_candle_stick
-
-__all__ = [
-    '_vnquant_candle_stick_source', '_vnquant_candle_stick'
-]
